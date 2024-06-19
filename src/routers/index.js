3b6d540ed55614a081b13b4339ceae7937a1d35f
thang9047 <thang04116@gmail.com>
2024-06-20 06:16:00 +0700

    Check API Key - Permission

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,15 @@
 'use strict'
 
 const express = require('express')
+const { route } = require('./access')
+const { apiKey, permission } = require('../auth/checkAuth')
 
 const router = express.Router()
 
-
+// Check Api Key
+router.use(apiKey)
+// Check permission
+router.use(permission('0000'))
 router.use('/v1/api', require('./access'))
 
 module.exports = router
